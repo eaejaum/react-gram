1fc76380e59897f8204674c8a3b820bdaa360cc4
eaejaum <jpedroalmeida179@gmail.com>
2024-10-28 22:21:03 -0300

    return dbConn

--- backend/config/db.js
+++ backend/config/db.js
@@ -7,6 +7,8 @@ const conn = async () => {
     try {
         const dbConn = await mongoose.connect(`mongodb+srv://${dbUser}:${dbPassword}@cluster0.sfl80.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`);
         console.log("Conectou ao banco!")
+        
+        return dbConn;
     } catch (error) {
         console.log(error);
     }
