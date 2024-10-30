cef721229888aba814fb8dd4831cc9c77b8240be
eaejaum <jpedroalmeida179@gmail.com>
2024-10-29 23:20:05 -0300

    initializing validations

--- backend/routes/UserRoutes.js
+++ backend/routes/UserRoutes.js
@@ -4,7 +4,10 @@ const router = express.Router();
 // controller
 const { register } = require("../controllers/UserController");
 
+// middlewares
+const validate = require("../middlewares/handleValidation");
+
 // routes
-router.post("/register", register);
+router.post("/register", validate, register);
 
 module.exports = router;
