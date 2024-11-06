c61ccb0975dc66a3bc8f248d1ba6298c4b3d01bc
eaejaum <jpedroalmeida179@gmail.com>
2024-11-06 09:57:22 -0300

    user login

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -7,50 +7,71 @@ const jwtSecret = process.env.JWT_SECRET;
 
 // generate user token
 const generateToken = (id) => {
-    return jwt.sign({ id }, jwtSecret, {
-        expiresIn: "7d",
-    });
+  return jwt.sign({ id }, jwtSecret, {
+    expiresIn: "7d",
+  });
 };
 
 // register user and sign in
-const register = async(req, res) => {
-    const {name, email, password} = req.body;
+const register = async (req, res) => {
+  const { name, email, password } = req.body;
 
-    // check if user exists
-    const user = await User.findOne({email})
-    if(user) {
-        res.status(422).json({ errors: ["Por favor, utilize outro e-mail"]})
-        return    
-    }
+  // check if user exists
+  const user = await User.findOne({ email });
+  if (user) {
+    res.status(422).json({ errors: ["Por favor, utilize outro e-mail"] });
+    return;
+  }
 
-    //generate password hash
-    const salt = await bcrypt.genSalt();
-    const passwordHash = await bcrypt.hash(password, salt);
+  //generate password hash
+  const salt = await bcrypt.genSalt();
+  const passwordHash = await bcrypt.hash(password, salt);
 
-    //create user
-    const newUser = await User.create({
-        name,
-        email,
-        password: passwordHash
-    })
+  //create user
+  const newUser = await User.create({
+    name,
+    email,
+    password: passwordHash,
+  });
 
-    // if user was created successfully, return the token
-    if(!newUser) {
-        res.status(422).json({errors: ["Houve um erro, por favor tente novamente mais tarde.w"]})
-        return
-    }
-    res.status(201).json({
-        _id: newUser._id,
-        token: generateToken(newUser._id)
-    })
+  // if user was created successfully, return the token
+  if (!newUser) {
+    res.status(422).json({
+      errors: ["Houve um erro, por favor tente novamente mais tarde.w"],
+    });
+    return;
+  }
+  res.status(201).json({
+    _id: newUser._id,
+    token: generateToken(newUser._id),
+  });
 };
 
 // sign user in
-const login = (req, res) => {
-    res.send("Login")
-}
+const login = async (req, res) => {
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
+
+  //check if user exists
+  if (!user) {
+    res.status(404).json({ errors: ["Usuário não encontrado."] });
+    return;
+  }
+
+  //check if password matches
+  if (!(await bcrypt.compare(password, user.password))) {
+    res.status(422).json({ errors: ["Senha inválida."] });
+    return;
+  }
+
+  res.status(201).json({
+    _id: user._id,
+    profileImage: user.profileImage,
+    token: generateToken(user._id),
+  });
+};
 
 module.exports = {
-    register,
-    login,
-}
+  register,
+  login,
+};
