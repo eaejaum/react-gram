cf11a8ccedc9d0f1cb132641ba3f541ae746b18d
eaejaum <jpedroalmeida179@gmail.com>
2024-11-06 10:31:36 -0300

    update user middleware

--- backend/middlewares/userValidations.js
+++ backend/middlewares/userValidations.js
@@ -36,13 +36,25 @@ const loginValidation = () => {
       .withMessage("O e-mail é obrigatório.")
       .isEmail()
       .withMessage("Insira um e-mail válido."),
+    body("password").isString().withMessage("A senha é obrigatória."),
+  ];
+};
+
+const userUpdateValidation = () => {
+  return [
+    body("name")
+      .optional()
+      .isLength({ min: 3 })
+      .withMessage("O nome precisa de pelo menos 3 caracteres."),
     body("password")
-      .isString()
-      .withMessage("A senha é obrigatória.")
+      .optional()
+      .isLength({min: 5})
+      .withMessage("A senha precisa ter no mínimo 5 caracteres"),
   ];
 };
 
 module.exports = {
   userCreateValidation,
   loginValidation,
+  userUpdateValidation
 };
