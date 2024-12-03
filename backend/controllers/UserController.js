91f7317ed6ea58995e5ae153009256480fecfd42
eaejaum <jpedroalmeida179@gmail.com>
2024-12-02 21:21:31 -0300

    get user by id

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -87,9 +87,9 @@ const update = async (req, res) => {
     profileImage = req.file.filename;
   }
   const reqUser = req.user;
-  const user = await User.findById(new mongoose.Types.ObjectId(reqUser._id)).select(
-    "-password"
-  );
+  const user = await User.findById(
+    new mongoose.Types.ObjectId(reqUser._id)
+  ).select("-password");
   if (name) {
     user.name = name;
   }
@@ -98,16 +98,36 @@ const update = async (req, res) => {
     const salt = await bcrypt.genSalt();
     const passwordHash = await bcrypt.hash(password, salt);
 
-    user.password = passwordHash
+    user.password = passwordHash;
   }
-  if(profileImage) {
+  if (profileImage) {
     user.profileImage = profileImage;
   }
-  if(bio) {
+  if (bio) {
     user.bio = bio;
   }
-  await user.save()
+  await user.save();
   res.status(200).json(user);
+};
+
+const getUserById = async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const user = await User.findById(mongoose.Types.ObjectId(id)).select(
+      "-password"
+    );
+
+    // check if user exists
+    if (!user) {
+      res.status(404).json({ errors: ["Usuário não encontrado."] });
+      return;
+    }
+
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(422).json({ errors: ["Usuário não encontrado."] });
+  }
 };
 
 module.exports = {
@@ -115,4 +135,5 @@ module.exports = {
   login,
   getCurrentUser,
   update,
+  getUserById,
 };
