517fbbfaf448bc224e66923b4736c8a2d9f38779
eaejaum <jpedroalmeida179@gmail.com>
2024-12-03 19:03:57 -0300

    fixing cors route to localhost

--- backend/app.js
+++ backend/app.js
@@ -13,7 +13,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 
 // solve CORS
-app.use(cors({credentials: true, origin: 'http://localhost3000'}));
+app.use(cors({credentials: true, origin: 'http://localhost:3000'}));
 
 // upload directory
 app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
