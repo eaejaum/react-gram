1fb3925210ae7f9a57c3e69349ab11fe29f1fb0c
eaejaum <jpedroalmeida179@gmail.com>
2024-12-02 23:33:19 -0300

    search photos

--- backend/controllers/PhotoController.js
+++ backend/controllers/PhotoController.js
@@ -168,6 +168,13 @@ const commentPhoto = async (req, res) => {
   });
 };
 
+const searchPhotos = async (req, res) => {
+  const { q } = req.query;
+  const photos = await Photo.find({ title: new RegExp(q, "i") }).exec();
+
+  res.status(200).json(photos);
+};
+
 module.exports = {
   insertPhoto,
   deletePhoto,
@@ -177,4 +184,5 @@ module.exports = {
   updatePhoto,
   likePhoto,
   commentPhoto,
+  searchPhotos
 };
