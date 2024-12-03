2ed44b9c66a31f377267db4d6ed57c982a505c85
eaejaum <jpedroalmeida179@gmail.com>
2024-12-02 22:50:48 -0300

    get user photos

--- backend/controllers/PhotoController.js
+++ backend/controllers/PhotoController.js
@@ -34,37 +34,50 @@ const deletePhoto = async (req, res) => {
   const { id } = req.params;
   const reqUser = req.user;
 
-    const photo = await Photo.findById(new mongoose.Types.ObjectId(id));
+  const photo = await Photo.findById(new mongoose.Types.ObjectId(id));
 
-    //Check if photo exists
-    if (!photo) {
-      res.status(404).json({ errors: ["Foto não encontrada!"] });
-      return;
-    }
+  //Check if photo exists
+  if (!photo) {
+    res.status(404).json({ errors: ["Foto não encontrada!"] });
+    return;
+  }
 
-    //Check if photo belongs to user
-    if (!photo.userId.equals(reqUser._id)) {
-      res.status(422).json({
-        errors: ["Ocorreu um erro, por favor tente novamente mais tarde."],
-      });
-    }
+  //Check if photo belongs to user
+  if (!photo.userId.equals(reqUser._id)) {
+    res.status(422).json({
+      errors: ["Ocorreu um erro, por favor tente novamente mais tarde."],
+    });
+  }
 
-    await Photo.findByIdAndDelete(photo._id);
+  await Photo.findByIdAndDelete(photo._id);
 
-    res
-      .status(200)
-      .json({ id: photo._id, message: "Foto excluída com sucesso." });
+  res
+    .status(200)
+    .json({ id: photo._id, message: "Foto excluída com sucesso." });
 };
 
 //Get all Photos
 const getAllPhotos = async (req, res) => {
-    const photos = await Photo.find({}).sort([["createdAt", -1]]).exec()
+  const photos = await Photo.find({})
+    .sort([["createdAt", -1]])
+    .exec();
 
-    return res.status(200).json(photos)
-}
+  return res.status(200).json(photos);
+};
+
+//Get user photos
+const getUserPhotos = async (req, res) => {
+  const { id } = req.params;
+  const photos = await Photo.find({ userId: id })
+    .sort([["createdAt", -1]])
+    .exec();
+
+    return res.status(200).json(photos);
+};
 
 module.exports = {
   insertPhoto,
   deletePhoto,
-  getAllPhotos
+  getAllPhotos,
+  getUserPhotos,
 };
